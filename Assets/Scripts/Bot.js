a108ca4037f4c4f94b23ff5d10c2a8934a9e6bd8
FoxelFox <storkner@gmail.com>
2014-06-23 07:40:46 +0200

    Added cheater Bot

---
+++ Assets/Scripts/Bot.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var Ball : Transform;
+var speed : float = 10;
+
+function Start () {
+
+}
+
+function Update () {
+	if(transform.position.y < Ball.position.y) {
+		rigidbody2D.velocity.y = speed;
+	} else {
+		rigidbody2D.velocity.y = speed *-1;
+	}
+}
